2844a8131a48382ef69895682a53865fbc23b128
Tradeface <niels.berkers@tradeface.nl>
2020-05-19 21:25:05 +0200

    removed stacktrace on exceptions

--- tuyaface/tuyaclient.py
+++ tuyaface/tuyaclient.py
@@ -45,7 +45,6 @@ class TuyaClient(threading.Thread):
                 self._reset_pong()
         except socket.error:
             self.force_reconnect = True
-            pass
 
 
     def _reset_pong(self):
@@ -93,8 +92,7 @@ class TuyaClient(threading.Thread):
                             try:
                                 self.connection.close()
                             except Exception:
-                                logger.exception("TuyaClient: exception when closing socket")
-                                pass
+                                logger.exception("TuyaClient: exception when closing socket", exc_info=False)
                             self.connection = None
 
                     if self.connection == None:
@@ -103,8 +101,7 @@ class TuyaClient(threading.Thread):
                             self._connect()
                             logger.info("TuyaClient: connected")
                         except Exception:
-                            logger.exception("TuyaClient: exception when opening socket")
-                            pass
+                            logger.exception("TuyaClient: exception when opening socket", exc_info=False)
 
                     if self.connection:
                         # poll the socket, as well as the socketpair to allow us to be interrupted
@@ -121,9 +118,8 @@ class TuyaClient(threading.Thread):
                                             reply = json.loads(reply["data"])
                                             self.on_status(reply)
                             except socket.error:
-                                logger.exception("TuyaClient: exception when reading from socket")
+                                logger.exception("TuyaClient: exception when reading from socket", exc_info=False)
                                 self.force_reconnect = True
-                                pass
 
                         if self.socketpair[0] in can_read:
                             # Clear the socket's buffer
