efe267fd8bb8d655110a888258429401a0281071
Erik <erik@montnemery.com>
2020-06-03 21:16:06 +0200

    Bump version to 1.3.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-version_tuple = (1, 3, 0)
+version_tuple = (1, 3, 1)
 version = version_string = __version__ = "%d.%d.%d" % version_tuple
 __author__ = "tradeface"
 
